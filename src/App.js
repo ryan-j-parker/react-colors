b9d51f99c8073acdca770606b892552c3dc46e2f
Ryan J. Parker <ryan.jos.parker@gmail.com>
2023-01-09 14:44:04 -0800

    migrate color router to webpack and react router v6

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import './App.css';
 import NotFound from './NotFound';
 import Main from './Main';
 import Header from './Header';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-
   return (
     <>
       <Header />
-      <Switch>
-        <Route exact path="/rgb/:red/:green/:blue">
-          <Main />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route exact path="/rgb/:red/:green/:blue" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    publicPath: '/',
+    clean: true,
+    filename: 'app.bundle.js',
+    path: __dirname + '/dist',
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(svg|png)$/,
+        use: 'url-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public', to: '.', }
+      ],
+    }),
+  ],
+};
